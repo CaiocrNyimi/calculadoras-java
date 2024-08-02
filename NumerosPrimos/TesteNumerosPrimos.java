fcba5e897801bf62de73b801d84686d3684b0ddf
CaiocrNyimi <161722010+CaiocrNyimi@users.noreply.github.com>
2024-08-02 17:05:35 -0300

    Add files via upload

---
+++ NumerosPrimos/TesteNumerosPrimos.java
@@ -0,0 +1,15 @@
+package overrideEExtends.NumerosPrimos;
+
+public class TesteNumerosPrimos {
+    public static void main(String[] args) {
+        VerificadorPrimo verificador = new VerificadorPrimo();
+        verificador.verificarSeEPrimo(17);
+
+        GeradorPrimo gerador = new GeradorPrimo();
+        int proximoPrimo = gerador.gerarProximoPrimo(17);
+        System.out.println("O próximo primo após 17 é: " + proximoPrimo);
+
+        NumerosPrimos numerosPrimos = new NumerosPrimos();
+        numerosPrimos.listarPrimos(30);
+    }
+}
---
+++ NumerosPrimos/VerificadorPrimo.java
@@ -0,0 +1,11 @@
+package overrideEExtends.NumerosPrimos;
+
+public class VerificadorPrimo extends NumerosPrimos {
+    public void verificarSeEPrimo(int numero) {
+        if (verificarPrimalidade(numero)) {
+            System.out.println(numero + " é primo.");
+        } else {
+            System.out.println(numero + " não é primo.");
+        }
+    }
+}
